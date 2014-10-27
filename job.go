0c28d26e257fb5dd4f0f4ecb3027f1341efe6e51
Cole Gleason <colewashere@gmail.com>
2014-10-27 01:00:15 -0500

    graph: add some stubs

---
+++ job.go
@@ -0,0 +1,22 @@
+package main
+
+type Credentials struct {
+	Username string
+	Password string
+}
+
+type PushInfo struct {
+	Image       string
+	Credentials Credentials
+}
+
+type Job struct {
+	Name         string
+	Requires     []*Job
+	Dockerfile   string
+	ImageName    string
+	DisableCache bool
+	SkipPush     bool
+	Tags         string
+	PushInfo     PushInfo
+}
