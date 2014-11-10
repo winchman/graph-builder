95ae6ffcb1b1875917150ce7e058272cf7308482
Cole Gleason <colewashere@gmail.com>
2014-11-10 16:56:36 -0600

    tests: moved example.go to graph_test.go

---
+++ buildgraph/graph_test.go
@@ -0,0 +1,53 @@
+package buildgraph_test
+
+import (
+	"testing"
+
+	"github.com/sylphon/graph-builder/buildgraph"
+)
+
+func TestValidGraph(test *testing.T) {
+	// Valid graph
+	a := buildgraph.Job{
+		Name: "Block-A",
+	}
+	b := buildgraph.Job{
+		Name:     "Block-B",
+		Requires: []*buildgraph.Job{&a},
+	}
+	c := buildgraph.Job{
+		Name:     "Block-C",
+		Requires: []*buildgraph.Job{&b},
+	}
+	validJobs := []*buildgraph.Job{&a, &b, &c}
+
+	graph, err := buildgraph.NewGraph(validJobs)
+	if err != nil {
+		test.Error(err)
+	}
+
+	if !graph.Validate() {
+		test.Fail()
+	}
+}
+
+func TestInvalidGraph(test *testing.T) {
+	// Will fail due to cycle
+	d := buildgraph.Job{
+		Name: "Block-D",
+	}
+	e := buildgraph.Job{
+		Name:     "Block-E",
+		Requires: []*buildgraph.Job{&d},
+	}
+	d.Requires = []*buildgraph.Job{&e}
+	invalidJobs := []*buildgraph.Job{&d, &e}
+	graph, err := buildgraph.NewGraph(invalidJobs)
+	if err != nil {
+		test.Error(err)
+	}
+
+	if graph.Validate() {
+		test.Fail()
+	}
+}
