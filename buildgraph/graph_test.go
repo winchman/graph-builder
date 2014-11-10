afd1ae7bc848adfb2c28af7797ff21715d9d2a94
Cole Gleason <colewashere@gmail.com>
2014-11-10 16:56:36 -0600

    tests: moved example.go to graph_test.go

--- buildgraph/graph_test.go
+++ buildgraph/graph_test.go
@@ -21,9 +21,13 @@ func TestValidGraph(test *testing.T) {
 	}
 	validJobs := []*buildgraph.Job{&a, &b, &c}
 
-	_, err := buildgraph.NewGraph(validJobs)
+	graph, err := buildgraph.NewGraph(validJobs)
 	if err != nil {
 		test.Error(err)
+	}
+
+	if !graph.Validate() {
+		test.Fail()
 	}
 }
 
@@ -38,65 +42,12 @@ func TestInvalidGraph(test *testing.T) {
 	}
 	d.Requires = []*buildgraph.Job{&e}
 	invalidJobs := []*buildgraph.Job{&d, &e}
-	_, err := buildgraph.NewGraph(invalidJobs)
-	if err == nil {
-		test.Fail()
-	}
-}
-
-func TestGetDependants(test *testing.T) {
-	a := buildgraph.Job{
-		Name: "Block-A",
-	}
-	b := buildgraph.Job{
-		Name:     "Block-B",
-		Requires: []*buildgraph.Job{&a},
-	}
-	c := buildgraph.Job{
-		Name:     "Block-C",
-		Requires: []*buildgraph.Job{&b},
-	}
-	validJobs := []*buildgraph.Job{&a, &b, &c}
-
-	graph, err := buildgraph.NewGraph(validJobs)
+	graph, err := buildgraph.NewGraph(invalidJobs)
 	if err != nil {
 		test.Error(err)
 	}
 
-	output1, err := graph.GetDependants([]*buildgraph.Job{})
-	if err != nil {
-		test.Error(err)
-	}
-
-	if len(output1) != 1 || output1[0] != &a {
+	if graph.Validate() {
 		test.Fail()
 	}
-
-	output2, err := graph.GetDependants([]*buildgraph.Job{&a})
-	if err != nil {
-		test.Error(err)
-	}
-
-	if len(output2) != 1 || output2[0] != &b {
-		test.Fail()
-	}
-
-	output3, err := graph.GetDependants([]*buildgraph.Job{&a, &b})
-	if err != nil {
-		test.Error(err)
-	}
-
-	if len(output3) != 1 || output3[0] != &c {
-		test.Fail()
-	}
-
-	output4, err := graph.GetDependants([]*buildgraph.Job{&a, &b, &c})
-	if err != nil {
-		test.Error(err)
-	}
-
-	if len(output4) != 0 {
-		test.Fail()
-	}
-
 }
