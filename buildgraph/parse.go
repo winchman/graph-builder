58003066cd643515637f66aca5e513bf545af5bb
Cole Gleason <colewashere@gmail.com>
2014-11-18 01:17:35 -0600

    parse: add yaml parsing

---
+++ buildgraph/parse.go
@@ -0,0 +1,68 @@
+package buildgraph
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+)
+
+type jobSpec struct {
+	Job           `yaml:",inline"`
+	RequiresBlock []string `yaml:"requires"`
+}
+
+type configDocument struct {
+	Blocks []*jobSpec
+}
+
+func uniqueStrings(strings []string) bool {
+	check := make(map[string]bool)
+	for _, s := range strings {
+		if check[s] == true {
+			return false
+		}
+		check[s] = true
+	}
+	return true
+}
+
+func ParseGraphFromYAML(input []byte) (g *Graph, err error) {
+	config := configDocument{}
+
+	err = yaml.Unmarshal(input, &config)
+	if err != nil {
+		return
+	}
+
+	jobMap := make(map[string]*Job)
+	jobList := make([]*Job, len(config.Blocks))
+	// Ensure job names are unique
+	for i, b := range config.Blocks {
+		if jobMap[b.Name] != nil {
+			err = fmt.Errorf("More than one job with the name %s",
+				b.Name)
+			return
+		}
+		jobMap[b.Name] = &(b.Job)
+		jobList[i] = &(b.Job)
+	}
+
+	// Setup dependancies
+	for _, b := range config.Blocks {
+		if !uniqueStrings(b.RequiresBlock) {
+			err = fmt.Errorf("Job %s must contain unique requirements.",
+				b.Name)
+			return
+		}
+		for _, requiredName := range b.RequiresBlock {
+			if jobMap[requiredName] == nil {
+				err = fmt.Errorf("Job %s required by %s does not exist",
+					requiredName, b.Name)
+				return
+			}
+			jobMap[b.Name].Requires =
+				append(jobMap[b.Name].Requires, jobMap[requiredName])
+		}
+	}
+
+	return NewGraph(jobList)
+}
